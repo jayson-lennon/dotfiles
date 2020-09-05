83ffbaf7dec9bc93ba3a163eda3bdb4adcd8ef3b
Jayson Lennon <jayson@jaysonlennon.dev>
2020-09-05 09:41:39 -0700

    Add script to autostart recording setup

---
+++ .scripts/move-meter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wmctrl \
+  -r :SELECT: -b add,sticky,above \
+  -r :SELECT: -t 5 \
+  -r :SELECT: -e 0,1846,684,-1,-1
---
+++ .scripts/record-mode.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+jack_control start
+
+CARLA=$(ps -AF|awk '/carla\/carla/ {print $2}')
+if [ -z $OBS ]
+  then
+    carla ~/data/projects/config/carla.carxp &
+    sleep 2
+fi
+
+OBS=$(ps -AF|awk '/\/usr\/bin\/obs/ {print $2}')
+if [ -z $OBS ]
+  then
+    obs &
+    sleep 2
+fi
+
+# Map tablet to appropriate screen
+setup-tablet
+
+jack_disconnect 'system:capture_1' 'PulseAudio JACK Source:front-left'
+jack_disconnect 'system:capture_2' 'PulseAudio JACK Source:front-right'
+
+cd ~/data/projects/config && obs-scene-switcher
